1d338495822cbb41a90497858a72214f178a60a1
negiabhay98 <negiabhay98@gmail.com>
2015-10-28 11:38:05 -0700

    swag

--- Chick.java
+++ Chick.java
@@ -1,4 +1,27 @@
 class Chick implements Animal 
 {     
-     //your code here
+     private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound1, String sound2)     {         
+         myType = type;         
+         mySound = sound1;
+         int chance;
+         chance = (int)(Math.random()*2);
+         if (chance == 1)
+         {
+            mySound = sound1;
+         }
+         else 
+        {
+            mySound = sound2;
+        }
+
+     }     
+     public Chick()     
+     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,6 @@
-
 void setup()
 {
-	//your variable declarations here
+	Farm oldMacdonald = new Farm();
+    oldMacdonald.animalSounds();
 }
 
